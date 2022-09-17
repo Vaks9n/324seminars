7f91bb281885ca167cffed20b434af82007353e1
Nikita Lachugin <schel2009@yandex.ru>
2022-09-17 14:52:21 +0400

    Третье задание

---
+++ Homework01/Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу которая на вход принимает число и выдает является ли оно четным
+
+Console.WriteLine("Введите число: ");
+
+double numberA = Convert.ToInt32(Console.ReadLine());
+
+double numberB = numberA % 2;
+
+if (numberB == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является нечетным");
+}
+
