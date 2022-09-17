706bdd3001396d7f7ac6ca8fbe6c1ba883842e2a
Nikita Lachugin <schel2009@yandex.ru>
2022-09-17 15:15:57 +0400

    Task8

---
+++ Homework01/Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа 
+// от 1 до N
+
+Console.WriteLine("Введите число: ");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while(count <= numberA )
+{
+    int numberB = count % 2;
+    if (numberB == 1)
+    {
+        Console.Write("");
+    }
+    else
+    {
+        Console.WriteLine("Четное число " + count);
+    }
+    count = count +1;
+}
