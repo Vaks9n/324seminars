99262b8c2ea83fa7cfa9de255657dc53a178fa97
Nikita Lachugin <schel2009@yandex.ru>
2022-09-22 10:55:31 +0400

    Task10

---
+++ Homework02/Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхз
+// начное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 999 || number < 100)
+{
+    Console.WriteLine("Ввели неверное число");
+}
+else
+{
+    int number1 = number / 10 % 10;
+    Console.WriteLine(number1);
+
+}
+
