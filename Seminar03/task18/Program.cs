f591372169c4e681ab307ee37acce61e4426bbcd
Nikita Lachugin <schel2009@yandex.ru>
2022-09-26 23:43:54 +0400

    task20

---
+++ Seminar03/task18/Program.cs
@@ -0,0 +1,26 @@
+﻿// 18 Напишите программу, которая по заданному номеру четверти показывает диапазон
+// возможных координат;
+
+Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Диапазон x > 0 или y > 0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Диапазон x < 0 или y > 0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Диапазон x < 0 или y < 0");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Диапазон x > 0 или y < 0");
+}
+else 
+{
+    Console.WriteLine("Ввелие неверное значение");
+}
---
+++ Seminar03/task20/Program.cs
@@ -0,0 +1,28 @@
+﻿// 20 Напишите программу которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2 д пространстве
+//
+
+Console.WriteLine("Введите первую координату точки А: ");
+double numA1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую координату точки А: ");
+double numA2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первую координату точки B: ");
+double numB1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую координату точки B: ");
+double numB2 = Convert.ToInt32(Console.ReadLine());
+
+// double X1 = numB1 - numA1;
+// double X = X1 * X1;
+// double Y1 = numB2 - numA2;
+// double Y = Y1 * Y1;
+// double number1 = X + Y;
+// Console.WriteLine(Y);
+// double number = Math.Sqrt(number1);
+
+double res = Math.Sqrt(Math.Pow(numB1 - numA1, 2) + Math.Pow(numB2 - numA2, 2));
+
+Console.WriteLine("Расстояние между точками в 2д пространстве: ");
+Console.WriteLine($"{res:f2}");
