0960849b4ce9d475dbc7741abffc7bc43478f108
Nikita Lachugin <schel2009@yandex.ru>
2022-09-30 09:18:01 +0400

    task27

---
+++ Homework04/task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int reverse = 0;
+int sum = 0;
+Console.WriteLine(number / 10);
+for (int i = 0; i <= number; i++)
+{
+    reverse = number % 10;
+    number = number / 10;
+    sum = sum + reverse;
+}
+
+Console.WriteLine(sum);
