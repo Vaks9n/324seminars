67540930d076f0666f6017adb074e844160f1727
Nikita Lachugin <schel2009@yandex.ru>
2022-09-30 10:06:06 +0400

    task29

---
+++ Homework04/task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+//  и выводит их на экран. Данные вводятся с консоли пользователем
+
+Console.WriteLine("Введите восемь чисел массива: \t ");
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+    
+    
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
