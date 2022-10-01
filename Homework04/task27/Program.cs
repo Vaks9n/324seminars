fb861f559d4b0b5f027df43962f939c7ec86ce1a
Nikita Lachugin <schel2009@yandex.ru>
2022-10-01 12:33:16 +0400

    task27

--- Homework04/task27/Program.cs
+++ Homework04/task27/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int reverse = 0;
 int sum = 0;
-for (int i = 0; i <= number; i++)
+for (int i = -10; i < number; i++)
 {
     reverse = number % 10;
     number = number / 10;
