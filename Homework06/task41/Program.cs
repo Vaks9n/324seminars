144d01494b8549f098130d60b7bc57d4fd1fcdf0
Nikita Lachugin <schel2009@yandex.ru>
2022-10-05 10:26:39 +0400

    homework

---
+++ Homework06/task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//   Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Для выхода из программы введите stop. Вводите числа: ");
+bool per = true;
+int sum = 0;
+while ( per == true)
+{
+    
+    string number = Console.ReadLine();
+    if (number == "stop")
+    {
+        break;
+    }
+    int number1 = Convert.ToInt32(number);
+    if (number1 > 0)
+    {
+         sum = sum + 1;
+    }
+    
+}
+Console.WriteLine($"Количество чисел больше 1: " + (sum));
+
---
+++ Homework06/task43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения
+//  двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Найдем точку пересечения двух прямых.");
+Console.WriteLine("Уравнение y = k1 * x + b1, y = k2 * x + b2 ");
+
+Console.WriteLine("Введите число b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double spot1 = (b2-b1) / (k1-k2);
+double spot2 = ((k1*b2) - (k2*b1)) / (k1-k2);
+
+Console.WriteLine("Точка пересечения: (" + spot1 + " ; " + spot2 + ")");
