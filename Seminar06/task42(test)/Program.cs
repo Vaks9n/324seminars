385a93768fb72f837c1155b1bb12397ef081d6a6
Nikita Lachugin <schel2009@yandex.ru>
2022-10-10 14:46:05 +0400

    Sem7

---
+++ Seminar06/task42(test)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (number > 0)
+{
+    int number1 = number % 2;
+    sum = sum * 10 + number1;
+    number = number / 2;
+}
+
+Console.WriteLine(sum);
---
+++ Seminar07/task49/Program.cs
@@ -0,0 +1,75 @@
+﻿
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+    
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+int SumMainDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    int min;
+    if (matrix.GetLength(0) >= matrix.GetLength(1))
+    {
+        min = matrix.GetLength(1);
+    }
+    else
+    {
+        min = matrix.GetLength(0);
+    }
+    for (int i = 0; i < min; i++)
+    {
+        sum = sum + matrix[i, i];
+    }
+    return sum;
+}
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+
+
+Console.WriteLine(SumMainDiagonal(matr));
