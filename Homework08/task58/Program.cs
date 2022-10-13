c14397709609bf024150cde76ead0323f99fb349
Nikita Lachugin <schel2009@yandex.ru>
2022-10-13 12:33:54 +0400

    Homework8

---
+++ Homework08/task58/Program.cs
@@ -0,0 +1,56 @@
+﻿
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+int [,] matrix1  = new int [3,3]
+{
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+};
+int [,] matrix2  = new int [3,3]
+{
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+};
+int [,] resultmatrix = new int [3, 3];
+
+void CreateNewMatrix(int[,] matrix1, int[,] matrix2, int [,] resultmatrix )
+{
+    for (int i = 0; i < resultmatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultmatrix.GetLength(1); j++)
+        {
+            resultmatrix[i,j] = matrix1[i,j] * matrix2[i,j];
+        }
+    }
+}
+CreateNewMatrix(matrix1, matrix2, resultmatrix);
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+PrintArray(resultmatrix);
---
+++ Homework08/task60/Program.cs
@@ -0,0 +1,70 @@
+﻿// 60 Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// массив размером 2 x 2 x 2
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+Console.WriteLine("Введите размер массива ");
+Console.WriteLine("Введите X ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Z ");
+int Z = Convert.ToInt32(Console.ReadLine());
+
+if (X*Y*Z > 90)
+{
+    Console.WriteLine("Невозможно задать такой массив");
+    
+}
+int [,,] MyArr = new int[X, Y, Z];
+int [] numbers = new int[X*Y*Z];  
+bool foundNum = true;
+int countNum =0; 
+for (int i = 0; i < X; i++)
+    {
+        for (int j = 0; j < Y; j++)
+            {
+                for (int k = 0; k < Z; k++)
+                {
+                    while (foundNum)
+                    {
+                        MyArr[i,j,k] =  new Random().Next(10, 100);
+                        foundNum = false;
+                        for (int s =0 ; s <X*Y*Z; s++)  
+                        {
+                            if (numbers[s] == MyArr[i,j,k])
+                            {
+                                foundNum = true;
+                                break;
+                            }
+                        }
+                    }
+                    
+                    numbers[countNum] = MyArr[i,j,k];
+                    countNum++;
+                    foundNum = true;
+
+                }
+                
+            }
+        }
+
+        
+for (int i = 0; i < X; i++)
+    {
+        for (int j = 0; j < Y; j++)
+        {
+            for (int k = 0; k < Z; k++)
+            {
+                Console.Write($"{MyArr[i,j,k]} ({i}, {j}, {k})    ");
+            }
+                
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+    
