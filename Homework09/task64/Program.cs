d1e88fabe140d705e030dcf772f3f01141393667
Nikita Lachugin <schel2009@yandex.ru>
2022-10-15 17:45:14 +0400

    task66

--- Homework09/task64/Program.cs
+++ Homework09/task64/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int a = 1;
+void Print1toN(int n, int a)
+{
+    
+    if(a > n)
+    {
+        return;
+    }
+    else
+    {
+        Print1toN(n, a + 1 );
+        
+        Console.Write(a + " ");
+    }
+}
+ 
+
+
+int number = ReadNumber("Введите N");
+Print1toN(number);
---
+++ Homework09/task66/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = ReadNumber("Введите M: ");
+int n = ReadNumber("Введите N: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
